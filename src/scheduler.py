1e3f0cb643c7bc2f6dfb7fdab8f5b1c65d5b3844
edkudriavtsev <354409@niuitmo.ru>
2025-03-24 00:16:00 +0300

    Refactored hyperparameters

--- src/scheduler.py
+++ src/scheduler.py
@@ -5,31 +5,35 @@ import math
 
 
 class Scheduler(ABC):
-    def __init__(self, hyperparameters: dict[str, float]):
-        self.hyperparameters = hyperparameters
+    __AUXILIARY_PREFIX = "aux_"
 
     def get_step_value(self, current_argument: tuple[float, ...], iteration_number: int,
                        func: DerivableFunction) -> float:
-        pass
+        return 0
 
     def get_hyper_parameters(self) -> dict[str, float]:
-        return self.hyperparameters
+        return {key: float(value) for key, value in self.__dict__.items() if
+                not key.startswith(Scheduler.__AUXILIARY_PREFIX)}
 
 
 class ExponentialDecayScheduler(Scheduler):
-    def __init__(self, step0: float, lamda: float):
+    def __init__(self, step0: float, lamda: float) -> None:
         assert lamda > 0 and step0 > 0
-        super().__init__({"step0": step0, "lamda": lamda})
+        self.step0 = step0
+        self.lamda = lamda
 
-    def get_step_value(self, current_argument: tuple[float], iteration_number: int, func: DerivableFunction) -> float:
-        return self.hyperparameters["step0"] * math.exp(-self.hyperparameters["lamda"] * iteration_number)
+    def get_step_value(self, current_argument: tuple[float, ...], iteration_number: int,
+                       func: DerivableFunction) -> float:
+        return self.step0 * math.exp(-self.lamda * iteration_number)
 
 
 class PolynomialDecayScheduler(Scheduler):
-    def __init__(self, alpha: float = 1 / 2, beta: int = 1):
+    def __init__(self, alpha: float = 1 / 2, beta: float = 1):
         assert alpha > 0 and beta > 0
-        super().__init__({"alpha": alpha, "beta": beta})
+        self.alpha = alpha
+        self.beta = beta
 
-    def get_step_value(self, current_argument: tuple[float], iteration_number: int, func: DerivableFunction) -> float:
+    def get_step_value(self, current_argument: tuple[float, ...], iteration_number: int,
+                       func: DerivableFunction) -> float:
         h0 = 1 / math.sqrt(iteration_number + 1)
-        return h0 * ((self.hyperparameters["beta"] * iteration_number + 1) ** -self.hyperparameters["alpha"])
+        return h0 * ((self.beta * iteration_number + 1) ** -self.alpha)
