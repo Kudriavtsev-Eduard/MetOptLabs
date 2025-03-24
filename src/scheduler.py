538fa94adafa799a547c9ba4256cac859b67a5d5
Laptev Ivan <105213447+LaptevIvan@users.noreply.github.com>
2025-03-24 22:55:29 +0300

    Wrote test function for scheduler and some tests

--- src/scheduler.py
+++ src/scheduler.py
@@ -98,4 +98,29 @@ class DihotomyScheduler(Scheduler):
 
 
 
+"""
+    Testing function for scheduler
+"""
+
+
+def test(func: DerivableFunction, arg: tuple[float, ...], scheduler: Scheduler):
+    print("Start:", arg, "Value:", func.apply(*arg))
+    h = scheduler.get_step_value(arg, 0, func)
+    antigravity = multiply(func.get_gradient_at(*arg), -1)
+    print("Antigravity:", antigravity, "h:", h)
+    next_arg = add_point(arg, multiply(antigravity, h))
+    print("End:", next_arg, "Value:", func.apply(*next_arg))
+    print("============================")
+
+
+dihotomy_scheduler = DihotomyScheduler(10, 20)
+
+test(DerivableFunction(lambda x, y: (x - 8) ** 2 + 100 * y ** 2, (lambda x, y: 2 * (x - 8), lambda x, y: 200 * y)),
+     (5, 0), dihotomy_scheduler)
+
+test(DerivableFunction(lambda x, y: (x - 8) ** 2 + 100 * y ** 2, (lambda x, y: 2 * (x - 8), lambda x, y: 200 * y)),
+     (2, 7), dihotomy_scheduler)
+
+test(DerivableFunction(lambda x, y: 0.01 * x ** 2 + 10 * y ** 2, (lambda x, y: 0.02 * x, lambda x, y: 20 * y)),
+     (11, -12), dihotomy_scheduler)
 
