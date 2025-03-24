86d6bd24f70793f6265b8f39aab420801ebcaa40
Laptev Ivan <105213447+LaptevIvan@users.noreply.github.com>
2025-03-25 02:26:21 +0300

    Wrote test some tests

--- src/scheduler_test.py
+++ src/scheduler_test.py
@@ -1,5 +1,5 @@
 from src.functions import DerivableFunction
-from src.scheduler import DihotomyScheduler
+from src.scheduler import DihotomyScheduler, GolderRatioScheduler
 import scheduler
 import utilities
 
@@ -8,6 +8,7 @@ import utilities
 """
 
 def test(func: DerivableFunction, arg: tuple[float, ...], scheduler: scheduler.Scheduler):
+    print("test of scheduler:", scheduler.get_name())
     print("Start:", arg, "Value:", func.apply(*arg))
     h = scheduler.get_step_value(arg, 0, func)
     antigravity = utilities.multiply(func.get_gradient_at(*arg), -1)
@@ -27,4 +28,18 @@ test(DerivableFunction(lambda x, y: (x - 8) ** 2 + 100 * y ** 2, (lambda x, y: 2
 
 test(DerivableFunction(lambda x, y: 0.01 * x ** 2 + 10 * y ** 2, (lambda x, y: 0.02 * x, lambda x, y: 20 * y)),
      (11, -12), dihotomy_scheduler)
+
+
+
+golden_scheduler = GolderRatioScheduler(10, 20)
+
+test(DerivableFunction(lambda x, y: (x - 8) ** 2 + 100 * y ** 2, (lambda x, y: 2 * (x - 8), lambda x, y: 200 * y)),
+     (5, 0), golden_scheduler)
+
+test(DerivableFunction(lambda x, y: (x - 8) ** 2 + 100 * y ** 2, (lambda x, y: 2 * (x - 8), lambda x, y: 200 * y)),
+     (2, 7), golden_scheduler)
+
+test(DerivableFunction(lambda x, y: 0.01 * x ** 2 + 10 * y ** 2, (lambda x, y: 0.02 * x, lambda x, y: 20 * y)),
+     (11, -12), golden_scheduler)
+
 
