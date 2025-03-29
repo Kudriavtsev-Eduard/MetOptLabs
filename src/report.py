c170cfa8aca6fb344224797c3d4776b845f507a3
Giliazov Valerii <408444niu.itmo.ru>
2025-03-29 21:03:32 +0300

    structure changes

--- src/report.py
+++ src/report.py
@@ -1,4 +1,8 @@
+from dataclasses import dataclass, field
+from typing import Any
+
 import plotly.graph_objects as go
+import json
 
 from functions import Function
 
@@ -7,95 +11,123 @@ report.py
 Displays job done by GradientOptimizer. Builds a graph of functions.
 """
 
+DEFAULT_CONFIG_PATH = "../config/display_settings.json"
 
+
+@dataclass
 class Report:
+    _func: Function
+    _tracking: list[tuple[float, ...]]
+    _is_aborted: bool
+    _hyperparameters: dict[str, float]
+    _strategy_name: str
+    _config_path: str = DEFAULT_CONFIG_PATH
+    _config: dict = field(init=False)
 
-    def __init__(self, func: Function, display_range: range, tracking: list[tuple[float, ...]],
-                 is_aborted: bool, hyperparameters: dict[str, float], strategy_name: str) -> None:
-        self._func = func
-        self._display_range = display_range
-        self._tracking = tracking
-        self._is_aborted = is_aborted
-        self._hyperparameters = hyperparameters
-        self._strategy_name = strategy_name
-
-    def get_raw_tracking(self) -> list[tuple[float, ...]]:
-        return self._tracking
+    def __post_init__(self) -> None:
+        with open(self._config_path, "r") as config_file:
+            self._config = json.load(config_file)
 
     def display(self) -> None:
         match self._func.get_arg_count():
             case 2:
-                self.build_3d_graph()
+                self._build_3d_graph()
             case _:
                 raise NotImplementedError("Report supports only functions with 2 args.")
 
-    def build_3d_graph(self) -> None:
-        x_values, y_values = zip(*self._tracking)
-        z_rangevalues = [[self._func.apply(x, y) for x in self._display_range] for y in self._display_range]
-        markers = ['diamond']
-        if len(x_values) > 1:
-            markers += ['circle'] * (len(x_values) - 2) + ['diamond']
+    def get_raw_tracking(self) -> list[tuple[float, ...]]:
+        return self._tracking
 
-        table_alignment_x = 0.05
-        table_values = [
-            ["Iterations", f"{len(self._tracking) - 1}"],
-            ["Begin point", f"({self._tracking[0][0]}, {self._tracking[0][1]})"],
-            ["Begin F", f"{self._func.apply(*self._tracking[0])}"],
-            ["Min F", f"{self._func.apply(self._tracking[-1][0], self._tracking[-1][1])}"],
-            ["Argmin F", f"({self._tracking[-1][0]}, {self._tracking[-1][1]})"],
-            ["Strategy", self._strategy_name],
-            ["Aborted?", "YES" if self._is_aborted else "NO"],
-            ["Hyperparameters", ", ".join(f"{k}={v}" for k, v in self._hyperparameters.items())]
-        ]
-        table_header = dict(
-            values=["<b>Parameter</b>", "<b>Value</b>"],
-            fill_color='lightblue',
-            align='center',
-        )
-        table_cells = dict(
-            values=list(list(col) for col in zip(*table_values)),
-            fill_color='lavender',
-            align='left'
-        )
+    @staticmethod
+    def _format_point(point: tuple[float, ...]):
+        return "(" + ", ".join(map(str, point)) + ")"
 
-        left_max_proportion = self._get_max_column_proportion(table_values, 0)
-        right_max_proportion = self._get_max_column_proportion(table_values, 1)
-        fig = (go.Figure()
-            .add_trace(go.Surface(
-                x=list(self._display_range),
-                y=list(self._display_range),
-                z=z_rangevalues,
-                colorscale='Sunsetdark',
-                contours={
-                    "z": {
-                        "show": True,
-                        "start": min(map(min, z_rangevalues)),
-                        "end": max(map(max, z_rangevalues)),
-                        "size": 5
-                    }
-                },
-                opacity=0.5)
-            )
-            .add_trace(go.Scatter3d(
-                x=x_values,
-                y=y_values,
-                z=[self._func.apply(x, y) for x, y in self._tracking],
-                mode='markers+lines',
-                marker=dict(size=7, color='royalblue', symbol=markers),
-                line=dict(width=7, color='royalblue'))
-            )
-            .add_trace(go.Table(
-                domain=dict(x=[table_alignment_x, table_alignment_x + (left_max_proportion + right_max_proportion) / 200],
-                            y=[0.5, 1]),
-                header=table_header,
-                cells=table_cells,
-                columnwidth=[left_max_proportion, right_max_proportion]))
+    @staticmethod
+    def _get_max_column_proportion(lst: list[list[str]], column: int) -> int:
+        return max(map(len, map(lambda pair: pair[column], lst)))
+
+    def _get_settings(self, parent_dict: str) -> dict[str, Any]:
+        if parent_dict not in self._config:
+            raise KeyError("No parent dict with name " + parent_dict)
+        return self._config.get(parent_dict)
+
+    def _build_3d_graph(self) -> None:
+        fig = (
+            go.Figure()
+            .add_trace(self._get_graph(self._get_settings("graph")))
+            .add_trace(self._get_trace(self._get_settings("trace")))
+            .add_trace(self._get_table(self._get_settings("table")))
         )
         fig.update_layout(autosize=True)
         fig.show()
 
-    def _get_max_column_proportion(self, lst: list[list[str]], column: int) -> int:
-        return max(map(len, map(lambda pair: pair[column], lst)))
+    def _get_graph(self, settings: dict[str, Any]) -> go.Surface:
+        var_range = range(*settings["display_range_bounds"])
+        var_list = list(var_range)
+        z_rangevalues = [[self._func.apply(x, y) for x in var_range] for y in var_range]
+        return go.Surface(
+            x=var_list,
+            y=var_list,
+            z=z_rangevalues,
+            colorscale=settings["colorscale"],
+            contours={
+                "z": {
+                    "show": True,
+                    "start": min(map(min, z_rangevalues)),
+                    "end": max(map(max, z_rangevalues)),
+                    "size": settings["level_line_indent"]
+                }
+            },
+            opacity=settings["opacity"]
+        )
 
-# report = Report(Function(lambda x, y: 0.1 * x ** 2 + 3 * y ** 2), range(-10, 10),[(4, 8), (3, 5) , (3, 4), (1, 2), (0, 0)], True, {"a": 5.33, "b": 6.33}, "Name")
-# report.build_3d_graph()
+    def _get_table(self, settings: dict[str, Any]) -> go.Table:
+        table_values = [
+            ["Iterations", f"{len(self._tracking) - 1}"],
+            ["Begin point", self._format_point(self._tracking[0])],
+            ["Begin F", f"{self._func.apply(*self._tracking[0])}"],
+            ["Min F", f"{self._func.apply(*self._tracking[-1])}"],
+            ["Argmin F", self._format_point(self._tracking[-1])],
+            ["Strategy", self._strategy_name],
+            ["Aborted?", "YES" if self._is_aborted else "NO"],
+            ["Hyperparameters", ", ".join(f"{k}={v}" for k, v in self._hyperparameters.items())]
+        ]
+
+        x_alignment = settings["x_alignment"]
+        proportions = [self._get_max_column_proportion(table_values, i) for i in range(2)]
+        return go.Table(
+            domain=dict(x=[x_alignment, x_alignment + sum(proportions) / 200],
+                        y=settings["y_domain"]),
+            header=settings["header"],
+            cells=dict(values=list(list(col) for col in zip(*table_values)),
+                       fill_color=settings["fill_color"],
+                       align=settings["align"]),
+            columnwidth=proportions)
+
+    def _get_trace(self, settings: dict[str, Any]) -> go.Scatter3d:
+        x_values, y_values = zip(*self._tracking)
+        tracking_len = len(self._tracking)
+        default_marker = settings["default_marker"]
+        special_marker = settings["special_marker"]
+        marker_settings = settings["marker_params"].copy()
+        marker_settings.update({
+            "symbol": (
+                    [special_marker] +
+                    [default_marker] * (tracking_len - 2) +
+                    ([special_marker] if tracking_len > 1 else [])
+            )
+        })
+        return go.Scatter3d(
+            x=x_values,
+            y=y_values,
+            z=[self._func.apply(x, y) for x, y in self._tracking],
+            mode=settings["scatter_mode"],
+            marker=marker_settings,
+            line=settings["line_params"]
+        )
+
+
+# report = Report(Function(
+#     lambda x, y: 0.1 * x ** 2 + 3 * y ** 2), [(4, 8), (3, 5), (3, 4), (1, 2), (0, 0)], True, {"a": 5.33, "b": 6.33},
+#     "Name")
+# report.display()
