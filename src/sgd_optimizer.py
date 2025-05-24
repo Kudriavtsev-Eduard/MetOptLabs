f3a812b1c32826538f859597067c20c48cf541a3
EdKudriavtsev <eduard.kudrawzew@outlook.com>
2025-05-24 21:36:02 +0300

    Implemented sgd_optimizer

---
+++ src/sgd_optimizer.py
@@ -0,0 +1,18 @@
+import typing
+
+from src.break_checker import BreakChecker
+from src.gradient_optimizer import GradientOptimizer
+from src.report import Report
+from src.scheduler import Scheduler
+from src.functions import BatchAutomatedDerivableFunction, HyperFunction
+
+
+class StochasticGradientOptimizer:
+    def __init__(self, scheduler: Scheduler, break_checker: BreakChecker, hyper_func: HyperFunction, limit: int):
+        self.grad_optimizer = GradientOptimizer(scheduler, break_checker, limit)
+        self.hyper_func = hyper_func
+
+    def optimize(self, dataset: typing.Sequence[tuple[tuple[float, ...], float]], hyperparams_begin: tuple[float, ...],
+                 batch_size: int) -> Report:
+        to_optimize = BatchAutomatedDerivableFunction(self.hyper_func, dataset, batch_size)
+        return self.grad_optimizer.optimize(to_optimize, hyperparams_begin)
